755af69d74c7dd5f31d73a80d07db64bfdbde863
Alice2212 <hezealice@gmail.com>
2023-05-02 22:20:06 +0100

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import aboutBanner from "../assets/images/about-banner.png";
+import aboutVector from "../assets/images/about-vector.svg";
+import {FaPlay} from "react-icons/fa"
 
 const About = () => {
   return (
-    <div>
-      welcome to about
-    </div>
-  )
-}
+    <section id="about" className="about">
+      <div className="container">
+        <figure>
+          <img src={aboutBanner} alt="about-img" />
+          <img src={aboutVector} alt="vector-line" />
+          <button className="play-btn">
+            <FaPlay/>
+          </button>
+        </figure>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
