f3dcea07870ffd4006759197500f269eb0522f7c
Alice2212 <hezealice@gmail.com>
2023-05-04 12:22:03 +0100

    available departments

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 const Service = () => {
   return (
-    <div>
-      Service page
+    <div
+      className="w-screen h-screen"
+      style={{
+        background: "var(--gradient)",
+      }}
+    >
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <h2 className="my-4 text-5xl font-bold leading-tight text-center mb-8">
+          We Have Most of Popular Departments
+        </h2>
+        {/* department list */}
+        <div className="department-list">
+          
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
 export default Service
