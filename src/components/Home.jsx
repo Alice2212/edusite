dd5b6f50b4e0b3b687728cb78c7ee14d15f91974
Alice2212 <hezealice@gmail.com>
2023-05-22 13:43:27 +0100

    hero section responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,28 +16,30 @@ const Home = () => {
          background: "var(--gradient)",
        }}
      >
-       <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-         <img
-           className="lg:w-3/6 md:w-3/6 w-5/6 mb-10 object-cover object-center"
-           alt="A young lady sits, holding a pen and a notebook."
-           src={banner}
-         />
-         <div className="text-center lg:w-10/12 w-full text-black">
-           <h1 className="my-4 text-5xl font-bold leading-tight">
+       <div className="container mx-auto flex px-5 py-24 items-center justify-between flex-wrap lg:flex-nowrap">
+         <div className="lg:order-2 order-1 lg:ml-12">
+           <img
+             className="lg:w-5/6 md:w-3/6 w-5/6 mb-10 object-cover object-center mx-auto"
+             alt="A young lady sits, holding a pen and a notebook."
+             src={banner}
+           />
+         </div>
+         <div className="lg:w-5/12 w-full lg:order-1 order-2 text-center lg:text-left mx-auto">
+           <h1 className="text-6xl font-bold leading-tight text-[#121217]">
              Start Your Future Education
            </h1>
-           <p className="text-2xl mb-8">
+           <p className="text-xl mt-12  mb-8 mx-auto text-[#6f6f76]">
              Credibly redefine distinctive total linkage vis-a-vis multifunction
              data. Phosphorescently impact goal-oriented strategic
            </p>
            {showMore && (
-             <p className="text-2xl mb-8">
+             <p className="text-xl mb-8 mx-auto text-[#6f6f76]">
                Providing quality education for all is fundamental to creating a
                peaceful and prosperous world. Education gives people the
                knowledge and skills.
              </p>
            )}
-           <div className="flex justify-center mx-auto">
+           <div className="flex  mx-auto justify-center lg:justify-start">
              <button onClick={toggleReadMore} className="btn btn-primary">
                {showMore ? "Show Less" : " Discover More"}
              </button>
