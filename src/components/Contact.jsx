708052b1402708faf330d070418012505a8d7665
Alice2212 <hezealice@gmail.com>
2023-05-29 20:26:24 +0100

    contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ import contactBanner from "../assets/images/cta-banner.png";
 const Contact = () => {
   return (
     <section className="w-full h-[15rem] bg-[--raisin-black] ">
-      <div className="container flex flex-wrap mx-auto py-24 relative overflow-y-hidden z-10">
-        <div className="flex justify-center items-center">
+      <div className="container flex flex-wrap mx-auto py-18 relative overflow-y-hidden z-10">
+        <div className="flex justify-center items-center mt-8 ">
           <h1 className="text-white text-2xl font-bold leading-loose">
             Create Free Account & <br></br>get Register
           </h1>
@@ -14,16 +14,12 @@ const Contact = () => {
             src={arrow}
             alt=""
             className="
-            lg:visible md:invisible sm:invisible mt-20"
+            lg:visible invisible mt-20"
           />
           <button className="btn btn-primary">Register Now</button>
         </div>
-        <div className="absolute left-[70%] top-[25%] 	z-20 lg:visible md:invisible sm:invisible ">
-          <img
-            src={contactBanner}
-            alt=""
-            className="object-contain h-[300px] "
-          />
+        <div className="absolute left-[70%] top-[10%] lg:visible invisible ">
+          <img src={contactBanner} alt="" className="object-fill h-[300px]  " />
         </div>
       </div>
     </section>
