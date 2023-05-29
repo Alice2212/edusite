7e7b137d1986567e0a4fc3d0558af412805b2f39
Alice2212 <hezealice@gmail.com>
2023-05-29 20:56:32 +0100

    contact section responsiveness

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,21 +4,21 @@ import contactBanner from "../assets/images/cta-banner.png";
 
 const Contact = () => {
   return (
-    <section className="w-full h-[15rem] bg-[--raisin-black] ">
-      <div className="container flex flex-wrap mx-auto py-18 relative overflow-y-hidden z-10">
-        <div className="flex justify-center items-center mt-8 ">
-          <h1 className="text-white text-2xl font-bold leading-loose">
-            Create Free Account & <br></br>get Register
+    <section className="w-full bg-[--raisin-black] ">
+      <div className="container flex flex-wrap mx-auto   py-18 relative overflow-y-hidden z-10">
+        <div className="flex flex-wrap flex-row justify-center items-center my-16 ">
+          <h1 className="text-white text-3xl font-bold leading-loose lg:w-1/2 w-full ">
+            Create Free Account & get Register
           </h1>
           <img
             src={arrow}
             alt=""
             className="
-            lg:visible invisible mt-20"
+            lg:visible invisible md:mt-4 lg:mt-20 md:self-center"
           />
           <button className="btn btn-primary">Register Now</button>
         </div>
-        <div className="absolute left-[70%] top-[10%] lg:visible invisible ">
+        <div className="absolute left-[70%] -top-[5%] lg:visible invisible ">
           <img src={contactBanner} alt="" className="object-fill h-[300px]  " />
         </div>
       </div>
